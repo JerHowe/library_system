12bc8925325249eb94987f86b307f57d7b6c6494
Jeremiah Howe <jeremiah@arrayschool.com>
2017-11-16 09:30:51 -0700

    Init Commit

--- scripts/min/main-min.js
+++ scripts/min/main-min.js
@@ -1,3 +1,29 @@
 var bookTemplate = $('#templates .book')
+var bookTable = $('#bookTable')
+
+var libraryID = 127
+var baseURL = `https://floating-woodland-64068.herokuapp.com/libraries/${libraryID}`
+
+
+// The bookData argument is passed in from the API
+function addBookToPage(bookData) {
+  var book = bookTemplate.clone()
+  book.attr('data-id', bookData.id)
+  book.find('.bookTitle').text(bookData.title)
+  book.find('.bookImage').attr('src', bookData.image_url)
+  book.find('.bookImage').attr('alt', bookData.title)
+  bookTable.append(book)
+}
+
+var getBooksRequest = $.ajax({
+  type: 'GET',
+  url: `${baseURL}/books`,
+})
+
+getBooksRequest.done( (dataFromServer) => {
+  dataFromServer.forEach( (bookData) => {
+    addBookToPage(bookData)
+  } )
+} )
 
 
