c33869a3ada31ead82b568cce1b5fcdd49aeca1d
Jeremiah Howe <jeremiah@arrayschool.com>
2017-11-16 14:29:31 -0700

    Add book functionality

--- scripts/min/main-min.js
+++ scripts/min/main-min.js
@@ -1,5 +1,7 @@
 var bookTemplate = $('#templates .book')
+var borrowerTemplate = $('#templates .borrower')
 var bookTable = $('#bookTable')
+var borrowerTable = $('#borrowerTable')
 
 var libraryID = 127
 var baseURL = `https://floating-woodland-64068.herokuapp.com/libraries/${libraryID}`
@@ -15,6 +17,17 @@ function addBookToPage(bookData) {
   bookTable.append(book)
 }
 
+function addBorrowerToPage(borrowerData) {
+  var borrower = borrowerTemplate.clone()
+  borrower.attr('data-id', borrowerData.id)
+  borrower.find('.borrowerFirstName').text(borrowerData.firstname)
+  borrower.find('.borrowerLastName').text(borrowerData.lastname)
+  borrowerTable.append(borrower)
+}
+
+
+
+// Grabbing and displaying Books to the page
 var getBooksRequest = $.ajax({
   type: 'GET',
   url: `${baseURL}/books`,
@@ -25,5 +38,40 @@ getBooksRequest.done( (dataFromServer) => {
     addBookToPage(bookData)
   } )
 } )
+
+// Grabbing and displaying Borrowers to the page
+var getBorrowersRequest = $.ajax({
+  type: 'GET',
+  url: `${baseURL}/borrowers`,
+})
+
+getBorrowersRequest.done( (dataFromServer) => {
+  dataFromServer.forEach( (borrowerData) => {
+    addBorrowerToPage(borrowerData)
+  } )
+} )
+
+
+$('#createBookButton').on('click', (event) => {
+  var bookData = {}
+  bookData.title = $('.addBookTitle').val()
+  bookData.description = $('.addBookDescription').val()
+  bookData.image_url = $('.addBookDescription').val()
+
+  var createBookRequest = $.ajax({
+    type: 'POST',
+    url: baseURL + '/books',
+    data: {
+      book: bookData
+    }
+  })
+
+  createBookRequest.done( (dataFromServer) => {
+    addBookToPage(dataFromServer)
+    $('addBookModal').modal('hide')
+    $('#addBookForm')[0].reset()
+  })
+
+})
 
 
